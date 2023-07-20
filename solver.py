b495cb8a46dc5becc114322d95b27f6754bd8d19
susiaguilar <susanjoaguilar@gmail.com>
2023-07-20 00:05:32 -0600

    checking for correct numbers

--- solver.py
+++ solver.py
@@ -12,6 +12,32 @@ board = [
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
  
+  # checking the row
+  
+def valid(bo, num, pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+#check the colum (vertically)
+
+    for i in range(len(bo)):
+         if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+#check the cube 
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+         
+    
+            
+ 
 def print_board(bo):
      for i in range(len(bo)):
         if i % 3 ==0 and i != 0:
@@ -19,7 +45,7 @@ def print_board(bo):
              
         for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
-                print(str(" | ", end=" "))
+                print(" | ", end=" ")
                 
             if j == 8:
                 print(bo[i][j])
@@ -30,4 +56,11 @@ def print_board(bo):
                     
 print_board(board)
 
-        
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return(i, j)  #  row, col
+            
+            
