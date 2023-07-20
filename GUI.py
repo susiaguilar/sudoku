0f6859db05c8b65bf3794242a4ed066d3f2fb859
susiaguilar <susanjoaguilar@gmail.com>
2023-07-20 17:40:16 -0600

    adding code to start building

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from solver import solve, valid
 pygame.font.init()
 
 class Grid:
@@ -14,9 +15,78 @@ class Grid:
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
+    
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128,128,128))
+            win.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+
+    def draw_change(self, win, g=True):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
+
+        text = fnt.render(str(self.value), 1, (0, 0, 0))
+        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
 
 
-
+                
+# def solve(bo):
+#     find = find_empty(bo)
+#     if find:
+#         row, col = find
+#     else:
+#         return True
+        
+#     for i in range(1, 10):
+#         if valid(bo, i, (row, col)):
+#             bo[row][col] = i
+            
+#             if solve(bo):
+#                 return True
+            
+#             bo[row][col] = 0
+            
+#     return False
 
 def find_empty(bo):
     for i in range(len(bo)):
@@ -27,24 +97,24 @@ def find_empty(bo):
     return None
 
 
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
+# def valid(bo, num, pos):
+#     # Check row
+#     for i in range(len(bo[0])):
+#         if bo[pos[0]][i] == num and pos[1] != i:
+#             return False
 
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
+#     # Check column
+#     for i in range(len(bo)):
+#         if bo[i][pos[1]] == num and pos[0] != i:
+#             return False
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+#     # Check box
+#     box_x = pos[1] // 3
+#     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
-                return False
+#     for i in range(box_y*3, box_y*3 + 3):
+#         for j in range(box_x * 3, box_x*3 + 3):
+#             if bo[i][j] == num and (i,j) != pos:
+#                 return False
 
-    return True
+#     return True
